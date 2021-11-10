a705af0c8688707d050c032e287d1e5a573f9652
Consolata Njeri <conso4u@gmail.com>
2021-11-10 00:00:45 -0800

    basic navigations working

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,14 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#post-name').value.trim();
-  const needed_funding = document.querySelector('#post-funding').value.trim();
-  const description = document.querySelector('#post-desc').value.trim();
+  const title = document.querySelector('#post-title').value.trim();
+  const content = document.querySelector('#post-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  alert(title, content);
+  if (title && content) {
     const response = await fetch(`/api/posts`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ title: title, content: content }),
       headers: {
         'Content-Type': 'application/json',
       },
